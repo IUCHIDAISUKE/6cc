f6e920da881fa923ae363494c9e3db243aa86e66
DAISUKE <33daisuke07@gmail.com>
2021-07-28 16:18:57 +0900

    improoved error messages

--- main.c
+++ main.c
@@ -22,6 +22,21 @@ struct Token
 };
 
 Token *token;
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
 void error(char *fmt, ...)
 {
@@ -43,14 +58,14 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("not '%c", op);
+        error_at(token->str, "not '%c", op);
     token = token->next;
 }
 
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("not number");
+        error_at(token->str, "not number");
     int val = token->val;
     token = token->next;
     return (val);
@@ -70,11 +85,12 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
     return (tok);
 }
 
-Token *tokenize(char *p)
+Token *tokenize()
 {
     Token head;
     head.next = NULL;
     Token *cur = &head;
+    char *p = user_input;
 
     while (*p)
     {
@@ -95,7 +111,7 @@ Token *tokenize(char *p)
             cur->val = strtol(p, &p, 10);
             continue;
         }
-        error("can't tokenize");
+        error_at(p, "expected number");
     }
 
     new_token(TK_EOF, cur, p);
@@ -110,7 +126,8 @@ int main(int argc, char **argv)
         return (1);
     }
 
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize();
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
